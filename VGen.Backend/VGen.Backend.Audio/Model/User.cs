f7b99d2e94ab6bb321a4b408497b2c63e93535fc
Alexis Alulema <alexis.alulema@gmail.com>
2025-02-22 06:36:18 -0500

    Initial loading

---
+++ VGen.Backend/VGen.Backend.Audio/Model/User.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace VGen.Backend.Audio.Model;
+
+public class User
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+    [JsonProperty("email")]
+    public string Email { get; set; }
+    [JsonProperty("username")]
+    public string Username { get; set; }
+    [JsonProperty("limited")]
+    public bool Limited { get; set; }
+    [JsonProperty("trialCount")]
+    public int TrialCount { get; set; }
+    [JsonProperty("isBanned")]
+    public bool IsBanned { get; set; }
+    [JsonProperty("lastLoginDate")]
+    public string LastLoginDate { get; set; }
+    [JsonProperty("creationDate")]
+    public string CreationDate { get; set; }
+    [JsonProperty("authProvider")]
+    public string AuthProvider { get; set; }
+    [JsonProperty("role")]
+    public string Role { get; set; }
+}
---
+++ VGen.Backend/VGen.Backend.Audio/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace VGen.Backend.Audio;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "https://accounts.google.com";
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = "https://accounts.google.com",
+                    ValidateAudience = true,
+                    ValidAudience = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID"),
+                    ValidateLifetime = true
+                };
+            });
+    }
+}
